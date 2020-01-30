cb5ec4f4972d1f1d792cec7e5b5d7bfcf3a44ffb
Janna Hastings <janna.hastings@babraham.ac.uk>
2020-01-30 14:25:41 +0100

    Update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ontoutils',         # How you named your package folder (MyLib)
   packages = ['ontoutils'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A collection of utility scripts mainly for translating content from Excel spreadsheets into OWL ontologies',   # Give a short description about your library
   author = 'Janna Hastings',                   # Type in your name
   author_email = 'janna.hastings@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jannahastings/ontoutils',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jannahastings/ontoutils/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jannahastings/ontoutils/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['OWL', 'ontologies', 'Excel'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
 	  'openpyxl',
